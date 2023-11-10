19a9031359fec5ed7fcddfb3d3162d9ff867db1b
Brent <brent.england@sunshinecoast.qld.gov.au>
2023-11-10 11:57:58 +1000

    added VS2

--- scc/install.ps1
+++ scc/install.ps1
@@ -4,3 +4,10 @@ $wingetPackages | {
 
     . winget install $_
 }
+
+$extraParams = " --custom '--add Microsoft.VisualStudio.Workload.Azure,Microsoft.VisualStudio.Workload.Data,Microsoft.VisualStudio.Workload.NetCrossPlat,Microsoft.VisualStudio.Workload.NetWeb'"
+
+winget install 'Microsoft.VisualStudio.2022.Enterprise' $extraParams
+
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
